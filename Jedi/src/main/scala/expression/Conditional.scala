5c6f086a1f130c42cc9cd0a88a8edb9f5c3c5c1b
Madina <madina2200@gmail.com>
2021-04-21 06:53:44 +0600

    Jedi1

--- Jedi/src/main/scala/expression/Conditional.scala
+++ Jedi/src/main/scala/expression/Conditional.scala
@@ -2,14 +2,14 @@ package expression
 import context.{Environment, alu}
 import value.{Boole, Notification, Value}
 
-case class Conditional(val exp1: Expression, val exp2: Expression, val exp3: Expression = null) extends SpecialForm {
+case class Conditional(exp1: Expression, exp2: Expression, exp3: Expression = null) extends SpecialForm {
   override def execute(env: Environment): Value = {
     val res: Value = exp1.execute(env)
     if(!res.isInstanceOf[Boole]) return Notification.unspecified
     if(res.asInstanceOf[Boole].value)
       exp2.execute(env)
     else if(exp2 != null)
-      exp2.execute(env)
+      exp3.execute(env)
     else
       Notification.done
   }
